eece9704cc5019165761c9bbd20032eef58b594e
Nicolly <nicollysenai29@gmail.com>
2024-08-30 11:18:42 -0300

    foi feito testes na pagina e configurações

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      require("@cypress/code-coverage/task")(on, config);
+      return config;
       // implement node event listeners here
     },
-    baseUrl: "http://localhost:3000/",
+    baseUrl: 'http://localhost:3000',
   },
 });
